47d3aa2baa205d9495b644100adfeb0e1510ada3
Cristhian Daza <cristian.d.b@hotmail.com>
2025-10-19 17:50:34 -0500

    chore(default.vue, mocks.js): refactor theme handling and improve null checks

--- src/utils/demos/default.vue
+++ src/utils/demos/default.vue
@@ -1,10 +1,11 @@
 <!-- This component save theme "light" or "dark" in the localStorage  -->
+<!-- Key: 'theme'  -->
 <template>
   <tv-theme-button @change-theme="changeValue"/>
 </template>
 
 <script setup>
-import TvThemeButton from "@todovue/tv-the-button";
+import { TvThemeButton } from "@todovue/tv-the-button";
 
 const changeValue = (value) => {
   console.log(value); // return light or dark
